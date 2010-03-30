c08e6bbbc5ea2cee8217bd63015ece463b358573
Apostolos Mpessas <mpessas@gmail.com>
2010-03-30 17:29:38 +0300

    Add generic command-line options.

--- pimme.py
+++ pimme.py
@@ -14,37 +14,37 @@ from pim_cmd import PimCmd
 from pim_errors import InvalidCommandError
 
 def set_cmd_options():
-    usage = u'usage: %prog [options]'
-    description = u'Store PIM informaition for the user in encrypted form.'
+    usage = u'usage: %prog cmd name [options]'
+    description = u'Store PIM information for the user in encrypted form.'
     parser = optparse.OptionParser(usage=usage, description=description)
-    parser.add_option('-a', '--add', action='store_const',
-                      const=0, dest='acion',
-                      help=u'Add a new item.')
-    parser.add_option('-i', '--item', action='store_const',
-                      const=1, dest='action',
-                      help=u'Edit an existing item')
-    parser.add_option('-i', '--item', dest='item',
-                      help=u'Edit an existing item')
+    parser.add_option('-v', '--verbose', action='store_true',
+                      dest='verbose', help=u'Add a new item')
+    parser.add_option('-c', '--config', dest='config_file',
+                      help=u'Use CONFIG_FILE for configuration')
+    parser.add_option('-d', '--debug', action='store_true',
+                      dest='debug', help='Turn debugging on')
     return parser
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
+    parser = set_cmd_options()
+    (options, args) = parser.parse_args(argv)
+
     try:
         command = PimCmd()
         if len(argv) == 1:
             msg = u'You have not specified a command!'
             raise InvalidCommandError(msg)
-        command.cmd[argv[1]]()
-        # parser = set_cmd_options()
-        # (options, args) = parser.parse_args(argv)
-        
-        # if options.add:
+        command.cmd[argv[1]](argv[2])
     except InvalidCommandError, e:
         print e
         return -1
     except KeyError, e:
         print u'Action ' + unicode(e) + ' is not defined!'
+        return -1
+    except indexError, e:
+        print 'Not enough arguments!'
         return -1
     except Exception, e:
         print e
