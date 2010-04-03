81662468375a0e9fddbefa7d73ac58f0f22f4fc8
Apostolos Mpessas <mpessas@gmail.com>
2010-04-03 11:57:33 +0300

    Add setup script to install/uninstall pimme.

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# (Un)Install pimme from user's home directory.
+
+set -e
+
+function install_pimme {
+    # copy files to ~/lib/python/
+    mkdir -p ~/lib/python/pimme
+    cp *py ~/lib/python/pimme/
+    cp setup.sh ~/lib/python/pimme/
+
+    # make soft link to ~/bin/
+    mkdir -p ~/bin
+    ln -s ~/lib/python/pimme/pimme.py ~/bin/pimme
+    chmod u+x ~/bin/pimme
+}
+
+function uninstall_pimme {
+    # remove all files
+    rm -f ~/bin/pimme
+    rm -rf ~/lib/python/pimme
+}
+
+nargs=1
+trap uninstall INT KILL TERM
+
+if [ $nargs -ne $# ]; then
+    echo 'Use install(uninstall) to install(uninstall).'
+    exit -1
+fi
+
+if [ $1 = 'install' ]; then
+    install_pimme
+else
+    if [ $1 = 'uninstall' ]; then
+	uninstall_pimme
+    else
+	echo 'Wrong argument.'
+	exit -1
+    fi
+fi
+exit 0
